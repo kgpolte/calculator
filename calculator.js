8522b9a89a78602d10ef85a54a0c5c5e5df7a26c
Kenny Polte <120602839+kgpolte@users.noreply.github.com>
2023-01-09 09:14:54 -0800

    Add equals button functionality

--- calculator.js
+++ calculator.js
@@ -30,33 +30,65 @@ function operate(operator, a, b) {
 
 
 const numBtns = Array.from(document.querySelectorAll('.num-btn'));
-const operatorButtons = Array.from(document.querySelectorAll('operator'));
+const operatorButtons = Array.from(document.querySelectorAll('.operator'));
 const currentDisplay = document.getElementById('current');
-// const previousDisplay = document.getElementById('previous');
+const previousDisplay = document.getElementById('previous');
+const equalsBtn = document.getElementById('equals');
+const clearBtn = document.getElementById('clear');
+
+let previousNum = null;
+let currentNum = null;
+let currentOperator = null;
 
 
-/* Add event listeners for number buttons */
+/* Event listeners for number buttons */
 
 numBtns.forEach(button => {
     button.addEventListener('click', e => {
         const num = e.target.textContent;
         const displayText = currentDisplay.textContent;
-        if (displayText == '0') {
+        if (currentNum == null) {
             currentDisplay.textContent = num;
         } else {
             currentDisplay.textContent = displayText + num;
         }
+        currentNum = parseInt(currentDisplay.textContent);
     });
 });
 
 
-/* Add event listeners for operator buttons */
-
-let previousNum = null;
-let currentOperator = null;
+/* Event listeners for operator buttons */
 
 operatorButtons.forEach(button => {
+    console.log(button);
+
     button.addEventListener('click', e => {
         
+        if (!currentNum) return;
+
+        if (currentOperator) {
+            previousNum = operate(currentOperator, previousNum, currentNum);
+        } else {
+            previousNum = currentNum;
+        }
+
+        currentNum = null;
+        currentOperator = e.target.textContent;
+        currentDisplay.textContent = '0';
+        previousDisplay.textContent = `${previousNum} ${currentOperator}`;
     });
+});
+
+
+/* Event listeners for unique buttons */
+
+equalsBtn.addEventListener('click', e => {
+
+    if(!currentOperator) return;
+
+    currentNum = operate(currentOperator, previousNum, currentNum);
+    previousNum = null;
+    currentOperator = null;
+    previousDisplay.textContent = '=';
+    currentDisplay.textContent = currentNum;
 });
